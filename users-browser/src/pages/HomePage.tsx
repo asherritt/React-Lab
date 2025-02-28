52d34a303e698e62d085a27bcf30850f9abed649
Anthony Sherritt <anthony.sherritt@gmail.com>
2025-02-28 13:51:16 -0800

    changes to home page

--- users-browser/src/pages/HomePage.tsx
+++ users-browser/src/pages/HomePage.tsx
@@ -4,10 +4,8 @@ import { Typography, Container } from '@mui/material';
 const HomePage: React.FC = () => {
   return (
     <Container sx={{ textAlign: 'center', marginTop: 5 }}>
-      <Typography variant='h4'>Welcome to the App</Typography>
-      <Typography variant='body1'>
-        Use the navigation above to browse users.
-      </Typography>
+      <Typography variant='h4'>Welcome</Typography>
+      <Typography variant='body1'>This is a demo app.</Typography>
     </Container>
   );
 };
